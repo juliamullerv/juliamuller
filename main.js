391b0c29577c59420c5a2050613f59012310d2d0
juliamullerv <163138595+juliamullerv@users.noreply.github.com>
2024-03-12 09:43:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://static.wikia.nocookie.net/tkoc/images/d/d4/Mickey_Mouse.png/revision/latest?cb=20220908132757&path-prefix=pt-br"
+	
+
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://static.wikia.nocookie.net/dublagem/images/3/3a/Mickey_mouse-1097.jpg/revision/latest?cb=20221206155947&path-prefix=pt-br"
+}
